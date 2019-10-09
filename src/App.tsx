eb6d482956afe25ac1a7591f71a2788162144083
BaekGeunYoung <dvmflstm@naver.com>
2019-10-09 18:30:27 +0900

    making bubble chart using d3 in typescript syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BubbleChart from "./BubbleChart";
+const _ = require('underscore');
+
+const rawdata = _.map(_.range(50), () => {
+    return {
+        v: _.random(10, 100)
+    };
+});
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BubbleChart
+          data={rawdata}
+          width={800}
+          height={600}
+          useLabels={true}
+      />
     </div>
   );
 }
